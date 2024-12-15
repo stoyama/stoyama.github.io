90f096eb743021bfe0ad0fa7afaff4790387f4c7
Tatsuya Toyama <36795614+stoyama@users.noreply.github.com>
2024-12-15 22:07:03 +0900

    feat: update contents based on Nextjs Portfolio Starter Kit

---
+++ app/components/footer.tsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+  return (
+    <footer className="mb-16">
+      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
+        © {new Date().getFullYear()} T11A Licensed
+      </p>
+    </footer>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        My Portfolio
+      </h1>
+      <p className="mb-4">
+        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
+        Vim's keystroke commands and tabs' flexibility for personal viewing
+        preferences.`}
+      </p>
+      <div className="my-8">
+        <BlogPosts />
+      </div>
+    </section>
+  )
+}
