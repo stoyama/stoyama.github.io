e92db1258fd61bc7d4eac463b5f6363bd60ab642
Tatsuya Toyama <36795614+stoyama@users.noreply.github.com>
2024-12-16 11:33:41 +0900

    fix(next.config): fix the basePath to load Tailwind CSS

--- next.config.mjs
+++ next.config.mjs
@@ -12,8 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/home",
-  // basePath: "/nextjs-github-pages",
+  basePath: process.env.DEPLOYED_GITHUB_PATH || '',
 
   /**
    * Disable server-based image optimization. Next.js does not support
