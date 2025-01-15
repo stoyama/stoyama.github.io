110458f3e9cece7244840b7c8fe397285d1a693a
Tatsuya Toyama <36795614+stoyama@users.noreply.github.com>
2025-01-16 08:29:47 +0900

    feat(app/layout): update the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
+    default: 'Portfolio',
     template: '%s | Next.js Portfolio Starter',
   },
-  description: 'This is my portfolio.',
+  description: 'My portfolio.',
   openGraph: {
     title: 'My Portfolio',
     description: 'This is my portfolio.',
